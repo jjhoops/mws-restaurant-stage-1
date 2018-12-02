b19ec2eff47406ba0e994bf2e2a108095251bbb7
jbrownads@gmail.com <jbrownads@gmail.com>
2018-12-02 01:23:32 -0500

    Update restaurant detail for responsive design * Implement flexbox for detail, map, reviews, footer

--- js/main.js
+++ js/main.js
@@ -72,34 +72,22 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
  * Initialize leaflet map, called from HTML.
  */
 initMap = () => {
-        self.newMap = L.map('map', {
-            center: [40.722216, -73.987501],
-            zoom: 12,
-            scrollWheelZoom: false
-        });
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-            mapboxToken: 'pk.eyJ1Ijoiampob29wcyIsImEiOiJjanAwbm1rYmszNms2M3FybnMwODV3cml2In0.HOqV9rtUYC5vjAYRozk4YQ',
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: 'mapbox.streets'
-        }).addTo(newMap);
-
-        updateRestaurants();
-    }
-    /* window.initMap = () => {
-      let loc = {
-        lat: 40.722216,
-        lng: -73.987501
-      };
-      self.map = new google.maps.Map(document.getElementById('map'), {
+    self.newMap = L.map('map', {
+        center: [40.722216, -73.987501],
         zoom: 12,
-        center: loc,
-        scrollwheel: false
-      });
-      updateRestaurants();
-    } */
+        scrollWheelZoom: false
+    });
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+        mapboxToken: 'pk.eyJ1Ijoiampob29wcyIsImEiOiJjanAwbm1rYmszNms2M3FybnMwODV3cml2In0.HOqV9rtUYC5vjAYRozk4YQ',
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets'
+    }).addTo(newMap);
+
+    updateRestaurants();
+}
 
 /**
  * Update page and map for current restaurants.
@@ -187,25 +175,15 @@ createRestaurantHTML = (restaurant) => {
  * Add markers for current restaurants to the map.
  */
 addMarkersToMap = (restaurants = self.restaurants) => {
-        restaurants.forEach(restaurant => {
-            // Add marker to the map
-            const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
-            marker.on("click", onClick);
-
-            function onClick() {
-                window.location.href = marker.options.url;
-            }
-            self.markers.push(marker);
-        });
-
-    }
-    /* addMarkersToMap = (restaurants = self.restaurants) => {
-      restaurants.forEach(restaurant => {
+    restaurants.forEach(restaurant => {
         // Add marker to the map
-        const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-        google.maps.event.addListener(marker, 'click', () => {
-          window.location.href = marker.url
-        });
+        const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
+        marker.on("click", onClick);
+
+        function onClick() {
+            window.location.href = marker.options.url;
+        }
         self.markers.push(marker);
-      });
-    } */
+    });
+
+}
