5300c0028712435693c087ca4a52f3cb457d604a
jbrownads@gmail.com <jbrownads@gmail.com>
2018-12-02 23:43:34 -0500

    Add ServiceWork Caching Tweaked restaurant.html responsiveness * add cache install * add cache fetch * add "clone" as required

--- sw.js
+++ sw.js
@@ -1,12 +1,56 @@
-self.addEventListener('fetch', event => {
+const cacheId = "restaurant-v1";
+const cacheFiles = [
+    "/",
+    "/index.html",
+    "/restaurant.html",
+    "/css/styles.css",
+    "/js/dbhelper.js",
+    "/js/main.js",
+    "/js/restaurant_info.js",
+    "/js/register.js",
+    "/data/restaurants.json",
+    "/img/1.jpg",
+    "/img/2.jpg",
+    "/img/3.jpg",
+    "/img/4.jpg",
+    "/img/5.jpg",
+    "/img/6.jpg",
+    "/img/7.jpg",
+    "/img/8.jpg",
+    "/img/9.jpg",
+    "/img/10.jpg"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(caches.open(cacheId).then(cache => {
+            return cache.addAll(cacheFiles);
+        })
+        .catch(error => {
+            console.log("Caches open failed: " + error);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request)
-        .then(response => {
-            // Cache hit - return response
+        caches.match(event.request).then(response => {
             if (response) {
+                console.log('Found', event.request, ' in cache');
                 return response;
+            } else {
+                console.log('Could not find ', event.request, ' in cache, will fetch it');
+                return fetch(event.request)
+                    .then(response => {
+                        const cloneResponse = response.clone();
+                        caches.open(cacheId).then(cache => {
+                            cache.put(event.request, cloneResponse);
+                        })
+                        return response;
+                    })
+                    .catch(error => {
+                        console.log("Caches open failed: " + error);
+                    });
             }
-            return fetch(event.request);
         })
     );
 });
