fbbaa75f3f555c049377a0306692a51dfe263ca0
jbrownads@gmail.com <jbrownads@gmail.com>
2018-12-02 23:53:41 -0500

    Prepare for project submission * Update README * Remove MapBox key for submission

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
     });
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1Ijoiampob29wcyIsImEiOiJjanAwbm1rYmszNms2M3FybnMwODV3cml2In0.HOqV9rtUYC5vjAYRozk4YQ',
+        mapboxToken: 'Replace this text with MapBox key',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
             '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
